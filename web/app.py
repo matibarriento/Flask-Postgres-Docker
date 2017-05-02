1e69d42c9d2ba3a9173117a659dffb8cf07b6c83
Matias Alberto Barriento <matias.barriento@neoris.cxnetworks.net>
2017-05-02 17:47:04 -0300

    Some changes and cheatsheet

--- web/app.py
+++ web/app.py
@@ -5,6 +5,7 @@ from models import Post
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # import pdb; pdb.set_trace()
     if request.method == 'POST':
         text = request.form['text']
         post = Post(text)
@@ -13,6 +14,12 @@ def index():
     posts = Post.query.order_by(Post.date_posted.desc()).all()
     return render_template('index.html', posts=posts)
 
+
+@app.route('/single', methods=['GET'])
+def single():
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    single_posts = posts[:1]
+    return render_template('index.html', posts=single_posts)
 
 if __name__ == '__main__':
     app.run()
